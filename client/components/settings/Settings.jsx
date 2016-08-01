a09be491f6c1329e6191662a52e0af16f5b52832
kyle-rader <kyle@kylerader.ninja>
2016-08-01 09:03:34 -0700

    Add start of settings structure.

--- client/components/settings/Settings.jsx
+++ client/components/settings/Settings.jsx
@@ -10,9 +10,38 @@ Settings = React.createClass({
     render() {
         return (
         <div className="ui container grid">
-            <h1 className="ui header">
-                Settings
-            </h1>
+            <div className="one column row">
+                <div className="column">
+                    <h1 className="ui header">
+                        Settings
+                    </h1>
+                </div>
+            </div>
+
+            <div className="two column row">
+                <div className="two wide column">
+                    <div className="ui comapct vertical labeled icon menu">
+                        <a className="item">
+                            <i className="blue lock icon"></i>
+                            Security
+                        </a>
+                        <a className="item">
+                            <i className="green pencil icon"></i>
+                            Blog
+                        </a>
+                        <a className="item">
+                            <i className="yellow bell icon"></i>
+                            Notifications
+                        </a>
+                    </div>
+                </div>
+
+                <div className="fourteen wide column">
+                    <div className="ui raised segment">
+                        There's gonna be stuff here
+                    </div>
+                </div>
+            </div>
         </div>
         );
     }
