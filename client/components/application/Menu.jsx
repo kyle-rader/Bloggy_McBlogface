c71d4e4f5e5b0257e4939d01d406c3722492d058
kyle-rader <kyle@kylerader.ninja>
2016-08-17 23:46:55 -0700

    Publish user roles and close menu on item click.

--- client/components/application/Menu.jsx
+++ client/components/application/Menu.jsx
@@ -17,6 +17,9 @@ Menu = React.createClass({
   },
 
   componentDidMount() {
+  },
+
+  componentDidUpdate() {
     // Init Semantic-UI SideBar
     const reactRoot = $('#react-root > .app-root.pushable');
     const sideBar = reactRoot.find('> .ui.sidebar');
@@ -24,18 +27,43 @@ Menu = React.createClass({
     sideBar.sidebar({
       context: reactRoot[0]
     });
+
+    // Close Menu on item click.
+    let items = sideBar.find('a.item');
+    console.log(items);
+
+    items.on('click', (elem) => {
+      sideBar.sidebar('toggle');
+    });
   },
 
-  componentDidUpdate() {
+  adminMenu() {
+    return [
+      (
+      <a className="item" href="#" key="edit-btn">
+        <i className="blue pencil icon"></i>
+        Editor
+      </a>
+      ),
+      (
+      <a className="item" href="#" key="settings-btn">
+        <i className="orange settings icon"></i>
+        Settings
+      </a>
+      ),
+    ];
   },
 
   render() {
+    const adminMenu = this.data.isAdmin ? this.adminMenu() : null;
+
     return (
     <div className="ui inverted sidebar vertical labeled icon menu">
       <a className="item" href="/">
         <img className="ui centered mini image" src="/img/logo-white-128.png"/>
         Home
       </a>
+      {adminMenu}
     </div>
     );
   }
---
+++ server/publications/Users.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.publish(null, function() {
+    let currentUser = this.userId;
+
+    if (currentUser) {
+    return Meteor.users.find({
+        _id: currentUser
+    }, {
+    fields: {
+        // Default
+        emails: 1,
+        // Default
+        profile: 1,
+        // Created roles property
+        roles: 1
+    }
+      });
+    } else {
+        return this.ready();
+    }
+});
