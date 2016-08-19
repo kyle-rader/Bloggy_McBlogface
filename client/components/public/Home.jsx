bbcf71df4fa6f1a86f9a4866cac3c5713a3f9aa1
kyle-rader <kyle@kylerader.ninja>
2016-08-18 22:07:55 -0700

    Reformat title header

--- client/components/public/Home.jsx
+++ client/components/public/Home.jsx
@@ -4,15 +4,16 @@ Home = React.createClass({
 
     render() {
         return (
-        <div className="ui container grid">
-            <br/>
-            <div className="row">
-                <div className="text-center column">
-                    <img className="ui centered small image" src="/img/logo-512.png"/>
-                    <h1>{Meteor.settings.public.siteName}</h1>
+        <div className="ui container">
+            <div className="ui center aligned header">
+                <img className="ui tiny image" src="/img/logo-512.png"/>
+                <div className="content">
+                    <h1 className="ui h1">{Meteor.settings.public.siteName}</h1>
+                    <div className="sub header">
+                        <h3 className="ui h3">Code, Life, & Cool Stuff</h3>
+                    </div>
                 </div>
             </div>
-            <br/>
         </div>
         );
     }
