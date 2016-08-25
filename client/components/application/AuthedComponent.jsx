a033994d012b1ae3b739b452761c588be2c9b42e
Kyle Rader <kyle@kylerader.ninja>
2016-08-24 22:53:05 -0700

    Upgrade to Meteor@1.4.1.1

--- client/components/application/AuthedComponent.jsx
+++ client/components/application/AuthedComponent.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 
 AuthedComponent = class AuthedComponent extends React.Component {
   render() {
-
-    console.log(this.props);
-
     if (this.props.canView()) {
       return this.props.children;
     } else {
