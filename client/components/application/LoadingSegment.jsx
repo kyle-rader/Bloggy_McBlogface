a8fc4b455011d4d0ea6a2c5b3e016eaa4b7ef567
Kyle Rader <kyle@kylerader.ninja>
2016-08-24 23:39:05 -0700

    Refactor loading segment comp as ES6 class

--- client/components/application/LoadingSegment.jsx
+++ client/components/application/LoadingSegment.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-LoadingSegment = React.createClass({
-    render() {
-        return (
-        <div className="ui segment">
-            <div className="ui active dimmer">
-                <div className="ui big text loader">Loading</div>
-            </div>
-            <br/> <br/>
-            <br/> <br/>
-            <br/> <br/>
-        </div>
-        );
-    }
-});
+LoadingSegment = class LoadingSegment extends React.Component {
+render() {
+  return (
+    <div className="ui segment">
+      <div className="ui active dimmer">
+          <div className="ui big text loader">Loading</div>
+      </div>
+      <br/> <br/>
+      <br/> <br/>
+      <br/> <br/>
+    </div>
+    );
+  }
+}
