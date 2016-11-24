10b858fd29ef7bb4a87a43dad98e108bfed27d7e
Kyle Rader <kyle@kylerader.ninja>
2016-11-23 21:01:37 -0800

    Implement controlled for fields

--- client/components/blog-editor/post-editor.jsx
+++ client/components/blog-editor/post-editor.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import { Menu, Button, Icon, Form, Confirm } from 'semantic-ui-react';
+import { Menu, Button, Icon, Form, Confirm, Loader, Segment, Dimmer } from 'semantic-ui-react';
 
 import brace from 'brace';
 import AceEditor from 'react-ace';
@@ -14,7 +14,21 @@ PostEditor = class PostEditor extends Component {
     super(props);
     this.state = {
       confirmDelete: false,
+      title: '',
+      body: '',
     };
+
+    this._handleTitleChange = this._handleTitleChange.bind(this);
+    this._handleBodyChange = this._handleBodyChange.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.post) {
+      this.setState({
+        title: nextProps.post.title,
+        body: nextProps.post.body,
+      });
+    }
   }
 
   _savePost(e) {
@@ -49,38 +63,56 @@ PostEditor = class PostEditor extends Component {
     );
   }
 
-  _renderMenu() {
+  _handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
+
+  _handleBodyChange(e) {
+    this.setState({ body: e.target.value });
+  }
+
+  _renderEditForm() {
     return (
-     <Menu secondary>
-      <Menu.Item>
-        <Button basic color="green" onClick={(e) => this._savePost(e)}>
+    <Form>
+      <Form.Group>
+        <Form.Button basic color="green" onClick={(e) => this._savePost(e)}>
           <Icon name="save"/>
           Save
-        </Button>
-      </Menu.Item>
-
-      <Menu.Menu position="right">
-        <Menu.Item>
-          <Button basic color="red" onClick={(e) => this._openConfirmDelete(e)}>
-            <Icon name="trash"/>
-            Delete
-          </Button>
-        </Menu.Item>
-      </Menu.Menu>
-     </Menu>
+        </Form.Button>
+        <Form.Button basic color="red" onClick={(e) => this._openConfirmDelete(e)}>
+          <Icon name="trash"/>
+          Delete
+        </Form.Button>
+      </Form.Group>
+      <Form.Input label="Title" name="title" placeholder="Title..." value={this.state.title} onChange={(e, val) => this._handleTitleChange(e, val)}/>
+      <Form.TextArea label="Body" name="body" placeholder="Body..." value={this.state.body} onChange={(e, val) => this._handleBodyChange(e, val)}/>
+    </Form>
     );
   }
 
-  render() {
+  _renderMain() {
     return (
     <div>
-      <Form>
-        {this._renderMenu()}
-      </Form>
+      {this._renderEditForm()}
       {this._renderDeleteModal()}
     </div>
     );
   }
+
+  render() {
+    if (this.props.loading) {
+      return (
+      <Segment>
+        <Dimmer active>
+          <Loader>Loading</Loader>
+        </Dimmer>
+      </Segment>
+      );
+    } else {
+      return this._renderMain();
+    }
+  }
+
 };
 
 /* Make PostEditor a container */
